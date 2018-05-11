1da5ebdaaab7bd72713bfccf6d53cdd81149a5de
Edward <ed828a@gmail.com>
2018-05-11 22:06:48 +0930

    Initial commit

---
+++ app/src/main/java/com/example/edward/coderswag/Model/Model.kt
@@ -0,0 +1,8 @@
+package com.example.edward.coderswag.Model
+
+/**
+ * @param: image-- String, just store image file name
+ */
+class Category(val title: String, val image: String)
+
+class Product(val title: String, val price: String, val image: String)
