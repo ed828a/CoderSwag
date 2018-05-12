399772e217907785585c510284e4ff96ac4dd71a
Edward <ed828a@gmail.com>
2018-05-12 22:55:53 +0930

    complete customized ListView

--- app/src/main/java/com/example/edward/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/edward/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.edward.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.edward.coderswag.Adapters.CategoryAdapter
 import com.example.edward.coderswag.Model.Category
 import com.example.edward.coderswag.R
 import com.example.edward.coderswag.Services.DataService
@@ -10,16 +11,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+//    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter<Category>(this,
-                android.R.layout.simple_list_item_1,
-                DataService.categories)
+//        adapter = CategoryAdapter(this, DataService.categories)
 
-        listViewCategories.adapter = adapter
+        listViewCategories.adapter = CategoryAdapter(this, DataService.categories)
     }
 }
